814ed6eb99d2c9380465ad0cd17feb24e65b7c6b
aron skelton <aron@skelton.us>
2018-04-28 12:54:49 -0400

    final

--- index.js
+++ index.js
@@ -1,91 +1,130 @@
-var Word = require("./word.js");
-var Letter = require("./letter.js") 
-var inquirer = require("inquirer"); 
-let computerChoices = ["tacos", "pizza", "ramen", "burger", "cookies"]; 
-let count = 0; 
-let winCount=0;
-let guessCount=10;
-let guesses = [];
+let Word = require("./word.js");
 
-Word.prototype.splitWord = function(selectedWord) {
-    let selectedLetters=[];
-    let selectedLetters= selectedWord.split("");
+let Letter = require("./letter.js");
+
+let inquirer = require("inquirer");
+
+let computerChoices = ["taco", "burger", "pizza", "cookies", "panini", "sandwhich", "burrito"];
+
+let count = 0;
+
+let winCount = 0;
+
+let guessCount = 18;
+
+let guessSoFar = [];
+
+
+Word.prototype.splitWord = function (selectedWord) {
+    let selectedLetters = [];
+    selectedLetters = selectedWord.split("");
     this.matches = selectedLetters.length
     return selectedLetters
-}; 
+};
 
-Letter.prototype.findMatches = function (guess, computerArray){
-    let indexes = [], i; 
-    for (i= 0; i<computerArray.length; i++)
-    if(computerArray[i]===guess.value) {
+Letter.prototype.findMatches = function (userGuess, computerArray) {
+ 
+
+    let indexes = [],i;
+    for (i = 0; i <computerArray.length; i++)
+        if( computerArray[i]===userGuess.value) {
         indexes.push(i);
-    }
-    return indexes; 
+        }
+    return indexes;
 };
 
 
-//create and store the new word object
+// create and store new word object
 
 let computerWord = new Word(computerChoices);
-let ComputerGuess= computerWord.selectedWord; 
+let computerGuess = computerWord.selectedWord;
 
-computerArray = computerWord.splitWord(ComputerGuess);
+computerArray = computerWord.splitWord(computerGuess);
 
-displayArray = computerWord.emptyDisplayWord(computerArray.length); 
+
+displayArray = computerWord.emptyDisplayWord(computerArray.length);
 console.log(displayArray.join(""));
 
-function resetAll(){
-    count=0;
-    guessCount=10;
-    guesses[];
+
+function resetAll() {
+
+    count = 0;
+    guessCount=18;
+
+    guessSoFar= [];
+
+
 }
 
-let askQuestion = function(){
-    if(count<10){
-        inquirer.prompt([
-            { 
-                name:"userGuess",
-                message:"Choose a letter",
+let askQuestion = function () {
+    if (count <18) {
 
+
+        inquirer.prompt([
+            {
+                name: "userGuess",
+                message: "What letter do you choose? "
             }
-        ]).then(function(answers){
-            let newLetter=answers.userGuess; 
-            if(guessSoFar.indexOf(newLetter)< 0){
+
+           
+        ]).then(function (answers) {
+            //
+            let newLetter = answers.userGuess;
+
+            if(guessSoFar.indexOf(newLetter) < 0){
                 guessSoFar.push(newLetter);
             }
-            console.log("----Guesses so far---- [" + guessSoFar.join(",")+ "]----");
+
+            console.log("Letters Used[" +guessSoFar.join(",") + "]");
+
+            guessCount--;
+
+            console.log("Number of Guesses:" + guessCount + "");
             console.log("");
 
-            if (matches===0){
-                console.log("Chyeaah! You Win!!");
+            let matches = computerWord.CheckAnswer(newLetter,computerArray);
+           
+
+            displayArray = computerWord.displayLetters;
+
+            console.log(displayArray.join(""));
+            console.log("");
+
+            if (matches === 0) {
+                console.log("Chyeeeahhh You Da Best!");
+
                 resetAll()
             }
             else {
-                count ++;
+                count++;
                 askQuestion();
             }
-        });
+    });
+
     }
-    else{
+    else {
         inquirer.prompt([
-            {
-                type:"confirm",
-                message:"would you like to end game?",
-                name:"end",
-                default: true
+        {
+            type: "confirm",
+            message:" would you like to end game",
+            name: "end",
+            default: false
 
-            }
-        ]).then(function (answer) {
-            if (answer.confirm){
-                resetAll();
-            }
-            else {
-                console.log("You are literally the worst")
-                resetAll();
-            }
-        })
+        }
+    ]).then(function(answer) {
+        if (answer.confirm) {
+            resetAll();
+
+        }
+        else {
+            console.log("You are literally the worst!")
+            resetAll();
+        }
+
+    });
+
     }
 
-}; 
+};
 
 askQuestion();
